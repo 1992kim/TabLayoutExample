b97c3d7a4b65af2943f799af9d8b428f0f616fbd
1992kim <rladydrbs92@naver.com>
2017-08-22 17:18:16 +0900

    초기화면

--- TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/SplashActivity.java
+++ TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/SplashActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 /**
  * Created by kim on 2017-08-14.
  */
-
+//초기대기화면
 public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//시간지정
         try {
             Thread.sleep(4000);
 
